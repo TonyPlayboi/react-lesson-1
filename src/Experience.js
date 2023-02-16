ac0b740d25d9076d47f323101ada146a83e176b2
TonyPlayboi <79407323+TonyPlayboi@users.noreply.github.com>
2023-02-16 13:02:05 +0100

    bobr

--- src/Experience.js
+++ src/Experience.js
@@ -1,6 +1,6 @@
 import {  useThree, extend, useFrame } from "@react-three/fiber"
-import { useAnimations ,Clone ,useGLTF, GradientTexture ,softShadows, BakeShadows, useHelper, MeshReflectorMaterial ,Float , Text ,  Html, PivotControls,  TransformControls, OrbitControls, CameraShake, PerspectiveCamera, SoftShadows } from "@react-three/drei"
-import { useRef, useEffect } from "react"
+import { Cloud, Text3D,  useAnimations ,Clone ,useGLTF, GradientTexture ,softShadows, BakeShadows, useHelper, MeshReflectorMaterial ,Float , Text ,  Html, PivotControls,  TransformControls, OrbitControls, CameraShake, PerspectiveCamera, SoftShadows, Sparkles } from "@react-three/drei"
+import { useRef, useEffect, useState } from "react"
 import * as THREE from 'three'
 import { Perf } from "r3f-perf"
 import { DoubleSide } from "three"
@@ -19,23 +19,25 @@ export default function Experience ()
     const kowRef1 = useRef()
     const kowRef2 = useRef()
     const burgerRef = useRef()
-   
+    const cameraAnimation = useRef()
 
-   const krowa = useGLTF('./Cube5.glb')
+   const krowa = useGLTF('./Beaver2.glb')
    const burger = useGLTF('./burger.glb')
    const animations = useAnimations(krowa.animations, krowa.scene)
    console.log(krowa)
 
    useEffect(() =>
    {
-      const action = animations.actions.bujana
+      const action = animations.actions.sraka
       action.play()
+
+      
 
       window.setTimeout(() => 
       {
 
-        animations.actions.shrink.play()
-        animations.actions.shrink.crossFadeFrom(animations.actions.bujana, 1)
+        animations.actions.sraka.play()
+        animations.actions.sraka.crossFadeFrom(animations.actions.sraka, 1)
       }, 2000)
    }, [])
   
@@ -48,6 +50,14 @@ export default function Experience ()
 
   return <>
 
+  <PerspectiveCamera
+  ref={cameraAnimation}
+  makeDefault
+  position={[0,0,15]}
+  >
+
+  </PerspectiveCamera>
+
 <primitive ref={kowRef} object={ krowa.scene} scale={1 } position={[4,1,1]}
  ></primitive>
 
@@ -56,7 +66,19 @@ export default function Experience ()
      position="top-left">
   </Perf>
 
-    
+    <Sparkles
+    size={[20,20,20]}
+    scale={3}
+    count={40}
+    color= {'lightgreen'}
+    >
+
+    </Sparkles>
+
+    <Cloud
+    color="red"
+    ></Cloud>
+
   <OrbitControls  
      makeDefault>
   </OrbitControls>
@@ -86,9 +108,22 @@ export default function Experience ()
     intensity={5}>
   </pointLight>
    
-   
- 
-
+   <Float speed={10}>
+ <Text3D 
+      font="./fonts/helvetiker_regular.typeface.json"
+      position={[1,-3,0]}
+      size={ 0.75 }
+      height={ 0.2 }
+      curveSegments={ 12 }
+      bevelEnabled
+      bevelThickness={ 0.02 }
+      bevelSize={ 0.02 }
+      bevelOffset={ 0 }
+      bevelSegments={ 5 } >
+  Gejuch
+  <meshStandardMaterial color={'darkorange'}></meshStandardMaterial>
+ </Text3D>
+ </Float>
 
 
 
